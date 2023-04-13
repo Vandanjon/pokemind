4077886b313df009f36a934ee01e21c284f3ee11
Splagadou <benoit@vandanjon.fr>
2023-04-13 09:59:54 +0200

    interesting css grid effect to superpose images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,20 @@ import pokeball from "../assets/pokeball.png";
 
 const Card = ({ pokeName, image, onClick, index }) => {
 	return (
-		<article className="card" onClick={() => onClick(index)}>
-			<div className="front-face">
-				<img src={image} alt={pokeName} />
-			</div>
-			<div className="back-face">
-				<img src={pokeball} alt="pokeball" />
+		// <article className="flip-card" onClick={() => onClick(index)}>
+		<article className="flip-card">
+			<div className="flip-card-inner">
+				<div className="front-face">
+					<img src={image} alt={pokeName} />
+				</div>
+
+				<div className="back-face">
+					<img src={pokeball} alt="pokeball" />
+				</div>
 			</div>
 		</article>
 	);
 };
 
 export default Card;
+
